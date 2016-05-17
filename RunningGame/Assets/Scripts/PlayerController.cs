cd38bb0cd5725ab5056d16b0f0906e523ba51cff
HarrisonSmith-UTS <harrison.pj.smith@gmail.com>
2016-05-17 17:39:20 +1000

    Basic Game controls
    Added basic physics and controls. Added player and a single floor tile.
    Organised assets folder into subfolders for different objects.

---
+++ RunningGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public bool grounded = true;
+    public float jumpPower = 100;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //'Jump'
+
+        if (!grounded && GetComponent<Rigidbody2D>().velocity.y == 0)
+        {
+            grounded = true;
+        }
+        if (Input.GetButtonDown("Fire1") && grounded == true)
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.up * jumpPower);
+            grounded = false;
+        }
+    }
+
+    //Called regularly
+    void fixedUpdate()
+    {
+        
+    }
+}
