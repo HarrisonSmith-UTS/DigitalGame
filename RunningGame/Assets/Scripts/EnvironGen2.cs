eae8a8b499d62aa1fa3c2b62d37793acae20a445
HarrisonSmith-UTS <harrison.pj.smith@gmail.com>
2016-06-03 17:49:43 +1000

    Autogen working, in testing

---
+++ RunningGame/Assets/Scripts/EnvironGen2.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironGen2 : MonoBehaviour
+{
+    public float createPosX;
+    private float createPosY;
+
+    public GameObject[] environParts;
+    private GameObject lastCreatedObj;
+    private int enIndex;
+    private PooledObject2 poolScript;
+
+    private Camera mainCam;
+    private float rightSideOfCam;
+
+    // Use this for initialization
+    void Start()
+    {
+        mainCam = Camera.main;
+        enIndex = 0;
+        createPosY = 0;
+
+        rightSideOfCam = mainCam.aspect * mainCam.orthographicSize;
+
+        //Creates one object off the bat
+        lastCreatedObj = (GameObject)Instantiate(environParts[enIndex], new Vector3(createPosX, createPosY, 0), Quaternion.identity);
+
+        createObject();
+    }
+
+    void Update()
+    {
+        if (createPosX <  mainCam.transform.position.x + rightSideOfCam)
+        {
+            createObject();
+        }
+    }
+
+    public void objectBecameVisible()
+    {
+        createObject();
+    }
+
+    //Called when an environment piece moves onto screen, indicating a new one is ready to be created off-screen
+
+    void createObject()
+    {
+        //Increments by the last created object.
+        poolScript = lastCreatedObj.GetComponent<PooledObject2>();
+        createPosX += poolScript.halfObjectWidth;
+
+        //Chooses random object to create
+        enIndex = Random.Range(0, environParts.Length);
+
+        //Increments by the newly created object
+        poolScript = environParts[enIndex].GetComponent<PooledObject2>();
+        createPosX += poolScript.halfObjectWidth;
+        createPosY = poolScript.defaultPosition.y;
+
+        lastCreatedObj = (GameObject)Instantiate(environParts[enIndex], new Vector3(createPosX, createPosY, 0), Quaternion.identity);
+    }
+
+    //Pooling stuff
+    
+}
---
+++ RunningGame/Assets/Scripts/PooledObject2.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PooledObject2 : MonoBehaviour
+{
+    private EnvironGen2 environScript;
+    public bool specifyObjectWidth;
+    public float objectWidth;
+    public float halfObjectWidth;
+
+    public Vector3 defaultPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!specifyObjectWidth)
+        {
+            objectWidth = gameObject.GetComponent<Collider2D>().bounds.size.x;
+        }
+        //defaultPosition = transform.position;
+    }
+
+    //Allows the environment script to set this object up for generation properly
+    public void initialiseEnvironScript(EnvironGen2 script)
+    {
+        environScript = script;
+    }
+
+    void OnBecameVisible()
+    {
+        //tell environment generator that it can create another object
+        environScript.objectBecameVisible();
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+        //return to pool
+        //Somehow notify environment generator?
+        //environScript.objectBecameInvisible(gameObject);
+    }
+}
