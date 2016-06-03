3bc2776d1c2948be9731170e30fa290505ae9fc5
HiuYeungTse11848138 <HiuYeung.Tse@student.uts.edu.au>
2016-06-03 20:47:34 +1000

    created start scene and level scene

---
+++ RunningGame/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void changeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
